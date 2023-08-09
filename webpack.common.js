80900e479fb1d0eaf69dafd150af105f8ba1f458
zoltan-antal <zoltan_antal@outlook.com>
2023-08-09 12:01:46 +0200

    Setup Webpack

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name].[contenthash][ext]',
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/template.html',
+    })
+  ],
+};
