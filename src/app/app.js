1fb4dcf761fc18bb2adc7de3667e170c33d39137
zoltan-antal <zoltan_antal@outlook.com>
2023-08-10 12:42:30 +0200

    Add rudimentary search functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,37 @@
 import { getCurrentWeatherData, getForecastWeatherData } from './api/api-calls';
-import {
-  processCurrentWeatherData,
-  processForecastWeatherData,
-  processWeatherData,
-} from './utilities/process-weather-data';
+import { processWeatherData } from './utilities/process-weather-data';
 
 export default async function main() {
-  const currentWeatherDataRaw = await getCurrentWeatherData('Tokyo');
-  const foreCastWeatherDataRaw = await getForecastWeatherData('Tokyo');
+  // const currentWeatherDataRaw = await getCurrentWeatherData('Tokyo');
+  // const foreCastWeatherDataRaw = await getForecastWeatherData('Tokyo');
 
-  const weatherData = processWeatherData(
-    currentWeatherDataRaw,
-    foreCastWeatherDataRaw,
-  );
-  console.log(weatherData);
+  // const weatherData = processWeatherData(
+  //   currentWeatherDataRaw,
+  //   foreCastWeatherDataRaw,
+  // );
+  // console.log(weatherData);
+
+  const locationInput = document.getElementById('location-input');
+  const locationSearchButton = document.getElementById('location-search');
+  let weatherData;
+
+  locationSearchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    locationInput.value = '';
+
+    Promise.all([
+      getCurrentWeatherData(location),
+      getForecastWeatherData(location),
+    ])
+      // .catch((err) => {
+      //   throw err;
+      // })
+      .then((responses) => {
+        weatherData = processWeatherData(responses[0], responses[1]);
+        console.log(weatherData);
+      })
+      .catch(() => {
+        alert("Couldn't fetch weather data for that location.");
+      });
+  });
 }
