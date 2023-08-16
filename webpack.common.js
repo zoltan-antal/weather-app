3d516a0898bc9186dd0d0bdbc9cf9a8644216172
zoltan-antal <zoltan_antal@outlook.com>
2023-08-16 17:29:49 +0200

    Change Webpack clean setting

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    clean: true
   },
   module: {
     rules: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,5 +5,6 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'main.[contenthash].js',
+    clean: true,
   },
 });
