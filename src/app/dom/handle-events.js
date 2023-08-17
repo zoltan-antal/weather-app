806cd72375affe94a1370c44282b7eea76d1e209
zoltan-antal <zoltan_antal@outlook.com>
2023-08-17 16:29:32 +0200

    Factor out async functions

--- src/app/dom/handle-events.js
+++ src/app/dom/handle-events.js
@@ -9,22 +9,28 @@ const locationInput = document.querySelector('.search-input');
 const locationSearchForm = document.querySelector('.search');
 let weatherData;
 
-export default async function setUpEventListeners() {
+async function fetchWeatherData(location) {
+  Promise.all([
+    getCurrentWeatherData(location),
+    getForecastWeatherData(location),
+  ])
+    .then((responses) => {
+      weatherData = processWeatherData(responses[0], responses[1]);
+      console.log(weatherData);
+      localStorage.setItem('location', location);
+      displayWeatherData(weatherData);
+    })
+    .catch(() => {
+      alert("Couldn't fetch weather data for that location.");
+    });
+}
+
+export default function setUpEventListeners() {
   locationSearchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = locationInput.value;
     locationInput.value = '';
-    Promise.all([
-      getCurrentWeatherData(location),
-      getForecastWeatherData(location),
-    ])
-      .then((responses) => {
-        weatherData = processWeatherData(responses[0], responses[1]);
-        console.log(weatherData);
-        displayWeatherData(weatherData);
-      })
-      .catch(() => {
-        alert("Couldn't fetch weather data for that location.");
-      });
+
+    fetchWeatherData(location);
   });
 }
