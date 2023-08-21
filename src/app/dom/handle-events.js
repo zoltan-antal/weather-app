5ad81f59c143f2bcad4d7c7f499cf4eaff91c27a
zoltan-antal <zoltan_antal@outlook.com>
2023-08-21 10:18:47 +0200

    Add daily/hourly forecast toggle functionality

--- src/app/dom/handle-events.js
+++ src/app/dom/handle-events.js
@@ -4,6 +4,12 @@ import fetchWeatherData from '../async/async-functions';
 const locationInput = document.querySelector('.search-input');
 const locationSearchForm = document.querySelector('.search');
 
+const hourlyButton = document.querySelector('.hourly-button');
+const dailyButton = document.querySelector('.daily-button');
+
+const hourlyDiv = document.querySelector('.hourly');
+const dailyDiv = document.querySelector('.daily');
+
 export default function setUpEventListeners() {
   locationSearchForm.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -16,5 +22,21 @@ export default function setUpEventListeners() {
     } catch (error) {
       alert("Couldn't fetch weather data for that location.");
     }
+  });
+
+  hourlyButton.addEventListener('click', () => {
+    hourlyDiv.classList.remove('hidden');
+    dailyDiv.classList.add('hidden');
+
+    hourlyButton.classList.add('selected');
+    dailyButton.classList.remove('selected');
+  });
+
+  dailyButton.addEventListener('click', () => {
+    dailyDiv.classList.remove('hidden');
+    hourlyDiv.classList.add('hidden');
+
+    dailyButton.classList.add('selected');
+    hourlyButton.classList.remove('selected');
   });
 }
